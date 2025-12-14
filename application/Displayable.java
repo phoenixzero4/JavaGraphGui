e35903410404a04368fb83e91af257a14e598d9c
William Phoenix Paul <phoenixzero4@gmail.com>
2025-12-13 17:10:18 -0700

    Initial commit of existing local files

---
+++ application/Displayable.java
@@ -0,0 +1,9 @@
+package application;
+
+public interface Displayable {
+	public int getX();
+
+	public int getY();
+
+	public String getName();
+}
---
+++ module-info.java
@@ -0,0 +1,5 @@
+module US_Graph {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
